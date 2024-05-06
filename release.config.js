db8de8c1779e37b442b657984f4a39e694f15101
Raed <raed.bahri@esprit.tn>
2024-05-06 11:45:43 +0100

    fix: update gitignore

---
+++ release.config.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const pkg = require("./package.json");
+
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/npm",
+      {
+        assets: [...pkg.files, "package.json", "yarn.lock", "CHANGELOG.md"],
+      },
+    ],
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "yarn.lock", "CHANGELOG.md"],
+      },
+    ],
+  ],
+};
